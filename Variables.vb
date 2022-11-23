bdc2da18ed631f0817106577bbc2e617d5f9d0be
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-11-23 09:44:03 +0000

    push updates

--- Variables.vb
+++ Variables.vb
@@ -1,98 +1,98 @@
-﻿Module Variables
+﻿'Module Variables
 
-    'Main function for arithmetic operations
-    Sub Main()
-        Dim a, b, c As Double
-        a = 20.0
-        b = 40.0
-        c = a * b
+'    'Main function for arithmetic operations
+'    Sub Main()
+'        Dim a, b, c As Double
+'        a = 20.0
+'        b = 40.0
+'        c = a * b
 
-        Dim x, y As Long
-        Dim z As Double
+'        Dim x, y As Long
+'        Dim z As Double
 
-        x = 3
-        y = 200
-        z = x / y
+'        x = 3
+'        y = 200
+'        z = x / y
 
-        Console.WriteLine("x={0}, y={1}, z={2}", x, y, z)
-        Console.WriteLine("a={0}, b={1}, c={2}", a, b, c)
+'        Console.WriteLine("x={0}, y={1}, z={2}", x, y, z)
+'        Console.WriteLine("a={0}, b={1}, c={2}", a, b, c)
 
-    End Sub
-
-
-
-    'Sub Main()
-
-    '    'Lvalues
-    '    'You can assign 1 to L, like so:
-    '    Dim l As Integer = 1
-
-    '    'OR
-
-    '    '1 to L, like so:
-    '    l = 1
-
-
-    '    'RValues
-    '    'You can assign 1 to R, like so:
-
-
-    '    Const R As Boolean = False
+'    End Sub
 
 
 
-    '    'Dim Name As String
-    '    'Name = "Paul"
+'    'Sub Main()
 
-    '    'Dim message As String
-    '    'Console.Write("Enter a Messsage: ")
-    '    'message = Console.ReadLine
-    '    ''This writes an empty line and does not terminate
-    '    'Console.WriteLine()
-    '    'Console.WriteLine("Your message is: {0}", message)
-    '    ''Invoke the console.readline method again after
-    '    ''console writeline is executed
-    '    'Console.ReadLine()
-    '    'Console.ReadKey()
-    'End Sub
+'    '    'Lvalues
+'    '    'You can assign 1 to L, like so:
+'    '    Dim l As Integer = 1
 
-    'Sub Main()
-    '    'Doing more with variables
+'    '    'OR
 
-    '    'You can create a local variable and assign a type.
-    '    'Dim Pi As Double
-    '    'Pi = 3.141
+'    '    '1 to L, like so:
+'    '    l = 1
 
 
-    '    'There are two ways to intialize a variable
-    '    'Method 1
-
-    '    Dim StudentId As Integer
-    '    StudentId = 1
-    '    Dim StudentClass As String
-    '    StudentClass = "Maroon"
+'    '    'RValues
+'    '    'You can assign 1 to R, like so:
 
 
-    '    'Method 2
-    '    Dim StaffId As Integer = 1
-    '    Dim StaffOffice As String = "General Office"
+'    '    Const R As Boolean = False
 
-    '    'Method 1
-    '    'Dim x, y, z As Integer
 
-    '    ''Method 2
-    '    'Dim j As Integer
-    '    'Dim k As Integer
-    '    'Dim L As Integer
 
-    '    'x = 50
-    '    'y = 5
-    '    'z = x / y
+'    '    'Dim Name As String
+'    '    'Name = "Paul"
 
-    '    'Console.WriteLine("x={0}, y={1}, z={2}", x, y, z)
-    '    Console.WriteLine(StudentId)
+'    '    'Dim message As String
+'    '    'Console.Write("Enter a Messsage: ")
+'    '    'message = Console.ReadLine
+'    '    ''This writes an empty line and does not terminate
+'    '    'Console.WriteLine()
+'    '    'Console.WriteLine("Your message is: {0}", message)
+'    '    ''Invoke the console.readline method again after
+'    '    ''console writeline is executed
+'    '    'Console.ReadLine()
+'    '    'Console.ReadKey()
+'    'End Sub
 
-    '    Console.ReadLine()
-    'End Sub
+'    'Sub Main()
+'    '    'Doing more with variables
 
-End Module
+'    '    'You can create a local variable and assign a type.
+'    '    'Dim Pi As Double
+'    '    'Pi = 3.141
+
+
+'    '    'There are two ways to intialize a variable
+'    '    'Method 1
+
+'    '    Dim StudentId As Integer
+'    '    StudentId = 1
+'    '    Dim StudentClass As String
+'    '    StudentClass = "Maroon"
+
+
+'    '    'Method 2
+'    '    Dim StaffId As Integer = 1
+'    '    Dim StaffOffice As String = "General Office"
+
+'    '    'Method 1
+'    '    'Dim x, y, z As Integer
+
+'    '    ''Method 2
+'    '    'Dim j As Integer
+'    '    'Dim k As Integer
+'    '    'Dim L As Integer
+
+'    '    'x = 50
+'    '    'y = 5
+'    '    'z = x / y
+
+'    '    'Console.WriteLine("x={0}, y={1}, z={2}", x, y, z)
+'    '    Console.WriteLine(StudentId)
+
+'    '    Console.ReadLine()
+'    'End Sub
+
+'End Module
